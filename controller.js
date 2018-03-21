ffc65da32a24b0115c717da4b7b1b1e31c6bdd56
zy <18612332440@163.com>
2018-03-21 14:55:43 +0800

    拼图活动

---
+++ controller.js
@@ -0,0 +1,7 @@
+angular.module('AndSell.H5.Main').controller('pages_temporary_pTAct_Controller', function($scope, $state, $stateParams,modalFactory) {
+//
+	 modalFactory.setBottom(false);
+//	 $scope.navShow=false
+	 modalFactory.setTitle("云厨年货拼拼乐");
+
+});
